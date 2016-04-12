ed9d4320e171a1b02f450f6a61373ad26a0f0505
Dost Muhammad Shah <dostmhd@gmail.com>
2016-04-12 08:46:38 +0400

    Update pdu_test.c

--- pdu_codec/pdu_test.c
+++ pdu_codec/pdu_test.c
@@ -19,57 +19,58 @@ along with Multi-part-SMS-PDU-generator.  If not, see <http://www.gnu.org/licens
 #include <locale.h>
 #include <wchar.h>
 
-int main()
-{
-  int cport_nr=22,        /* /dev/ttyAMA0 for testing */
-      bdrate=38400;       /* 38400 baud */
+int main() {
+    int cport_nr = 22,        /* /dev/ttyAMA0 for testing */
+            bdrate = 38400;       /* 38400 baud */
 
-  unsigned char buf[4096] = {0};
-  char* str;
-  int t1, k;
+    unsigned char buf[4096] = {0};
+    char *str;
+    int t1, k;
 
-  char s[30] = {0};
+    char s[30] = {0};
 
-  char* message = "1, 2, 3, 4, 5, 6, 7, 8, 9 and 10, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 1, 2, 3, 4, 5, 6, 7, 8, 9 and 10, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100.";
-  //char* message = "1, 2, 3, 4, 5, 6, 7, 8, 9 and 10, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, !!";
-  //char* message1 = "ئهجگچج";  //Should be 07916998051030F2040B916998775706F80008410192810153610C06260647062C06AF0686062C
-                              //in encoding 2 0011000891797765800008A70CD8A6D987D8ACDAAFDA86D8AC
-  //char* message1 = "\x06\xA3";
-  char *message1 = "\x06\x33\x06\x44\x06\x4a\x06\x45"; 
-  //const wchar_t *name = L"r\u00e9sum\u00e9";
-  //setlocale(LC_CTYPE, "en_US.UTF-8");
-  //wprintf(L"name is %ls\n", name);
-  char* to = "97775608";
-  int alphabet = 0;
-  int flash = 0;
-  int with_udh = 0;
-  char* udh_data = "";
-  char* mode = "new";
-  int report = 0;
-  int validity = 167; // 1 day
-  int system_msg = 0;
-  int replace_msg = 0;
-  int to_type = 1;
-  char* smsc = "";
-  size_t msgs;
-  
-  char **pdu = encode_to_pdu(&msgs, to, message, strlen(message), alphabet, flash, report, mode, validity, replace_msg, system_msg, to_type, smsc);
+    char *message = "1, 2, 3, 4, 5, 6, 7, 8, 9 and 10, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 1, 2, 3, 4, 5, 6, 7, 8, 9 and 10, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100.";
+    //char* message = "1, 2, 3, 4, 5, 6, 7, 8, 9 and 10, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, !!";
+    //char* message1 = "ئهجگچج";  //Should be 07916998051030F2040B916998775706F80008410192810153610C06260647062C06AF0686062C
+    //in encoding 2 0011000891797765800008A70CD8A6D987D8ACDAAFDA86D8AC
+    //char* message1 = "\x06\xA3";
+    char *message1 = "\x06\x33\x06\x44\x06\x4a\x06\x45";
+    //const wchar_t *name = L"r\u00e9sum\u00e9";
+    //setlocale(LC_CTYPE, "en_US.UTF-8");
+    //wprintf(L"name is %ls\n", name);
+    char *to = "97775608";
+    int alphabet = 0;
+    int flash = 0;
+    int with_udh = 0;
+    char *udh_data = "";
+    char *mode = "new";
+    int report = 0;
+    int validity = 167; // 1 day
+    int system_msg = 0;
+    int replace_msg = 0;
+    int to_type = 1;
+    char *smsc = "";
+    size_t msgs;
 
-  for(k=0;k<msgs;k++){
-    printf("pdu%d = %s\n", (int)k, pdu[k]);
+    char **pdu = encode_to_pdu(&msgs, to, message, strlen(message), alphabet, flash, report, mode, validity,
+                               replace_msg, system_msg, to_type, smsc);
 
-  }
-    if(pdu){
+    for (k = 0; k < msgs; k++) {
+        printf("pdu%d = %s\n", (int) k, pdu[k]);
+
+    }
+    if (pdu) {
         free_pdu(&msgs, pdu);
     }
 
-  char **pdu1 = encode_to_pdu(&msgs, to, message1, strlen(message1), alphabet, flash, report, mode, validity, replace_msg, system_msg, to_type, smsc);
-    
-  for(k=0;k<msgs;k++){
-    printf("pdu1%d = %s\n", (int)k, pdu1[k]);
+    char **pdu1 = encode_to_pdu(&msgs, to, message1, strlen(message1), alphabet, flash, report, mode, validity,
+                                replace_msg, system_msg, to_type, smsc);
 
-  }
-    if(pdu1){
+    for (k = 0; k < msgs; k++) {
+        printf("pdu1%d = %s\n", (int) k, pdu1[k]);
+
+    }
+    if (pdu1) {
         free_pdu(&msgs, pdu1);
     }
 //  free_pdu(&msgs, pdu1);
@@ -112,6 +113,6 @@ int main()
     printf("There's a problem with that pdu. It isn't supported!\n");
   }
      */
-  
-  return(0);
+
+    return (0);
 }
